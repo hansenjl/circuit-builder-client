6853e094602025312d2fcb66c359435cf9ae04f6
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-28 13:06:36 -0700

    Adding styling for displaying problems

--- src/containers/Problems.js
+++ src/containers/Problems.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Problems.css';
 
 const Problems = (props) => (
-  <div>
+  <div className="ProblemsContainer">
     <h3>Choose from existing problems:</h3>
     {props.problems.map((problem,index) =>
-      <div key={index}>
+      <div className="ProblemCard" key={index}>
         <p>Difficulty: {problem.difficulty}</p>
         <p>Category: {problem.category}</p>
         <p># of Loops: {problem.loops.length}</p>

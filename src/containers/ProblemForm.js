04f48b6c67d37cdac1144c146b0932d7bc0144ef
hansenjl <jenn.leigh.hansen@gmail.com>
2017-10-02 14:06:12 -0700

    Transformed problem form to be contained in a table

--- src/containers/ProblemForm.js
+++ src/containers/ProblemForm.js
@@ -13,20 +13,35 @@ class ProblemForm extends Component {
     this.props.updateProblemFormData(currentProblemFormData)
   }
 
+  handleNestedChange = event => {
+    const nestedProblemFormData = Object.assign({}, this.props.problemFormData, {
+      loops: [{
+        resistors:[
+          {resistance: event.target.value}
+        ]
+      }]
+    })
+    this.props.updateProblemFormData(nestedProblemFormData)
+  }
+
   handleOnSubmit = event => {
     event.preventDefault()
     this.props.createProblem(this.props.problemFormData)
       .then(this.props.resetProblemForm)
   }
 
+  handleAddResistor = event => {
+    //add another row to the table form for the second resistor
+  }
+
   render() {
-    const { difficulty, category, tot_voltage, tot_resistance } = this.props.problemFormData;
+    const { difficulty, category, tot_voltage, tot_resistance, loops } = this.props.problemFormData;
 
 
     return(
-      <div>
+      <div className="FormContainer">
         <form onSubmit={this.handleOnSubmit} >
-          <div>
+         <div>
             <select name="category" value={category} onChange={this.handleOnChange}>
               <option value="series">Series</option>
               <option value="parallel">Parallel</option>
@@ -34,32 +49,58 @@ class ProblemForm extends Component {
             </select>
           </div>
           <div>
-            <label>Difficulty:</label>
-            <input
-              type="number"
-              name="difficulty"
-              onChange={this.handleOnChange}
-              value={difficulty}/>
-          </div>
-          <div>
-            <label>Voltage:</label>
-            <input
-              type="number"
-              name="tot_voltage"
-              onChange={this.handleOnChange}
-              value={tot_voltage}/>
-          </div>
-          <div>
-            <label>Total Resistance:</label>
-            <input
-              type="number"
-              name="tot_resistance"
-              onChange={this.handleOnChange}
-              value={tot_resistance}/>
-          </div>
-          <div>
-            <label id="tot_current">Current: {(tot_voltage / tot_resistance).toFixed(2)}</label>
-          </div>
+              <label>Difficulty:</label>
+              <input
+                type="number"
+                name="difficulty"
+                onChange={this.handleOnChange}
+                value={difficulty}/>
+            </div>
+          <table>
+            <thead>
+            <tr>
+              <th></th>
+              <th>Voltage</th>
+              <th>Current</th>
+              <th>Resistance</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>TOTAL</td>
+              <td>
+                <input
+                  type="number"
+                  name="tot_voltage"
+                  onChange={this.handleOnChange}
+                  value={tot_voltage}/>
+              </td>
+              <td>
+                {(tot_voltage / tot_resistance).toFixed(2)}
+              </td>
+              <td>
+                <input
+                type="number"
+                name="tot_resistance"
+                onChange={this.handleOnChange}
+                value={tot_resistance}/>
+              </td>
+            </tr>
+            <tr>
+              <td>Resistor 1</td>
+              <td></td>
+              <td></td>
+              <td>
+                <input
+                type="number"
+                name="resistance"
+                onChange={this.handleNestedChange}
+                value={loops[0].resistors[0].resistance}/>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+          <button onClick={this.handleAddResistor}>Add a Resistor</button>
           <input type="submit" value="Save"/>
         </form>
       </div>

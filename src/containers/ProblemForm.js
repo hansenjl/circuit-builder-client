960c25b5e097f8afbaf44de19e84aee4a976c445
hansenjl <jenn.leigh.hansen@gmail.com>
2017-10-10 14:46:13 -0700

    Added a form reset action whent he problem form component is about to mount

--- src/containers/ProblemForm.js
+++ src/containers/ProblemForm.js
@@ -10,6 +10,10 @@ import ComboCircuit from '../components/ComboCircuit';
 
 class ProblemForm extends Component {
 
+  componentWillMount(){
+    this.props.resetProblemForm
+  }
+
   handleOnChange = event => {
     const {name, value} = event.target;
     const currentProblemFormData = Object.assign({}, this.props.problemFormData, {
